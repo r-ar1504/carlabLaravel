185bb672ccf899fa89b2640416e7bbcea2294d38
rRios15041995 <rodolfo.r1504@gmail.com>
2018-01-23 00:15:11 -0600

    Controladores

--- app/Http/Controllers/API.php
+++ app/Http/Controllers/API.php
@@ -8,11 +8,119 @@ use App\Service;
 
 class API extends Controller
 {
-    
-    function services(Request $req){
-      $services = DB::table('services');
-      return response()->json([
-        'services' => $services
-      ]);
-    }
+  //<!--[Get All Services]-->//
+  function services(Request $req){
+
+    $services = DB::select('select * from Service');
+    return response()->json(['services' => $services]);
+
+  }
+
+  //<!--[Get Service ==> Categories]-->//
+  function get_categories(Request $req, String $service_id){
+
+    $subcategories = [];
+
+    $categories = DB::table('Category');
+    return response()->json(['services' => $services]);
+
+  }
+
+
+
+
 }
+
+// @require_GET
+// def createOrder(request):
+//     message = " "
+//     pusher.trigger('orders', 'new-order', {message: "Order Arrived"})
+//     return HttpResponse()
+//
+// @csrf_exempt
+// def createWorker(request):
+//     data = json.loads(request.body.decode('utf-8'))
+//
+//     worker = Worker(fireID = data['fireID'], name= data['name'], last_name = data['last_name'], email= data['email'], role= data['role'], status= 'off_duty', phone= data['phone'])
+//
+//     worker.save()
+//     pusher.trigger('orders', 'new-order', {model_to_dict(worker)})
+//     return HttpResponse();
+//
+// @csrf_exempt
+// def getWorker(request):
+//     data = json.loads(request.body.decode('utf-8'))
+//     print data
+//     worker = model_to_dict(Worker.objects.get(fireID = data['fireID']))
+//     workerID = data['fireID']
+//     role = worker['role']
+//
+//     orders = getWorkerOrders(workerID, role)
+//
+//     return JsonResponse({
+//     'worker': worker
+//     })
+//
+// @csrf_exempt
+// def getOrders(request):
+//     data = json.loads(request.body.decode('utf-8'))
+//
+//     orders = Order.objects.filter(worker = data['worker']).values('name', 'status', 'latitude', 'longitude', 'cat_name')
+//
+//     return JsonResponse({
+//     'data': list(orders)
+//
+//     })
+//
+// @csrf_exempt
+// def workerStatus(request):
+//     data = json.loads(request.body.decode('utf-8'))
+//
+//     worker = model_to_dict(Worker.objects.get(fireID = data['fireID']))
+//     worker_id = data['fireID']
+//
+//     if worker['status'] != 'on_dutty':
+//         Worker.objects.filter(fireID = worker_id).update( status = 'on_dutty' )
+//     else:
+//         Worker.objects.filter(fireID = worker_id).update( status = 'off_dutty' )
+//
+//
+//     new_status = model_to_dict( Worker.objects.get(fireID = data['fireID']) )
+//
+//     return JsonResponse({
+//     'data': new_status['status']
+//     })
+//
+// @csrf_exempt
+// def doneOrder(request):
+//     data = json.loads(request.body.decode('utf-8'))
+//
+//     order_id = data['id']
+//
+//     if order_id != 'terminada':
+//         Order.objects.filter(id=order_id).update(status='terminada')
+//     else:
+//         Order.objects.filter(fireID=worker_id).update(status='off_dutty')
+//
+//     new_status = Order.objects.filter(id=order_id).values(status)
+//     return JsonResponse({
+//     'data': list(order_id)
+//     })
+//
+// #Custom Reusable Functions<------------------------------------------------------------------------>
+//
+// #<!-- Fetch Orders By Worker ID -->
+// def getWorkerOrders(workerID, role):
+//
+//     orders = model_to_dict( Order.objects.get( worker = workerID, status = "active", service = role ) )
+//
+//     return orders
+//
+// #<!-- Fetch SubCategory By Category ID -->
+// def getSubCategories( category_id ):
+//
+//     sub_categories = model_to_dict( SubCategory.objects.get( category_id = category_id ) )
+//
+//     return sub_categories
+//
+// #<!------------------------------------------------------------------------------------------------>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/api/get_services', "API@services");
