af89350183a98efd2a8cb9b16e2add7a1a4231d6
rRios15041995 <rodolfo.r1504@gmail.com>
2018-01-25 15:18:54 -0600

    added Pusher provider

--- config/app.php
+++ config/app.php
@@ -163,7 +163,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Pusher\Laravel\PusherServiceProvider::class
+        Vinkla\Pusher\PusherServiceProvider::class,
 
         /*
          * Package Service Providers...
