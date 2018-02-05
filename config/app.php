271739e0f9752b8868c5c3c287095728fca1c42b
rRios15041995 <rodolfo.r1504@gmail.com>
2018-02-05 02:57:43 -0600

    s

--- config/app.php
+++ config/app.php
@@ -226,7 +226,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Pusher' => Pusher\Laravel\Facades\Pusher::class
+        'LaravelPusher' => Pusher\Laravel\Facades\Pusher::class
     ],
 
 ];
