06ca236d95c6fee3499cbc93c1df41d2c0de05a4
rRios15041995 <rodolfo.r1504@gmail.com>
2018-02-08 15:31:18 -0600

    s

--- app/Http/Controllers/API.php
+++ app/Http/Controllers/API.php
@@ -167,7 +167,7 @@ class API extends Controller
 
       }
 
-      return response()->json(['status' => '200', 'order_id' => $order_id, 'workers' => $worker_list, 'count' => $worker_list]);
+      return response()->json(['status' => '200', 'order_id' => $order_id, 'workers' => $worker_list, 'count' => count($worker_list)]);
 
     }else{
 
