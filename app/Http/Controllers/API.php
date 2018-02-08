52f1cf9ff3a308832d1ee5ea5389fdb5019df01a
rRios15041995 <rodolfo.r1504@gmail.com>
2018-02-08 07:24:44 -0600

    s

--- app/Http/Controllers/API.php
+++ app/Http/Controllers/API.php
@@ -91,7 +91,7 @@ class API extends Controller
    if ($worker->status != '0') {
      DB::table('Worker')->where('fireID', $fireID)->update(['status' => '0']);
    }else {
-     DB::table('Worker')->where('fireID', $fireID)->(['status' => '1']);
+     DB::table('Worker')->where('fireID', $fireID)->update(['status' => '1']);
    }
 
     $nworker = DB::table('Worker')->where('fireID', $fireID)->first();
@@ -177,7 +177,7 @@ class API extends Controller
       'status' => 1]);
 
       Pusher::trigger('order-'.$order-id, 'got-worker', ['worker' => $worker]);
-      
+
         return response()->json(['code' => '1');
     }
     // Pusher::trigger('new-orders', 'new-order',  ['order_object' => $data]);
