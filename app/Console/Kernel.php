018589b3115c82abbba41969e2acd9c724e0c8c9
rRios15041995 <rodolfo.r1504@gmail.com>
2018-02-11 20:48:11 -0600

    save

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -39,6 +39,8 @@ class Kernel extends ConsoleKernel
 
           foreach ($orders as $order) {
             $worker_list = DB::table('Worker')->where('status', 1)->where('role', $order->service_name)->get();
+            DB::table('Order')->where('id', '=' $order->id)->update(['status'=> "2"]);
+
             if (count($worker_list)>0) {
               foreach ($worker_list as $worker) {
                 Pusher::trigger("worker-".$worker->fireID, "new-order", ['order' => $order]);
@@ -50,7 +52,7 @@ class Kernel extends ConsoleKernel
         }else {
           echo "No Pending Orders";
         }
-      })->everyMinute();	
+      })->everyMinute();
     }
 
     /**
