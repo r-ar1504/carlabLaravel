a84d551d611f89adbbad08b4572b6f3a5dc31f1f
rRios15041995 <rodolfo.r1504@gmail.com>
2018-02-22 07:57:46 -0600

    com

--- app/conekta-php/lib/Conekta/Conekta.php
+++ app/conekta-php/lib/Conekta/Conekta.php
@@ -4,7 +4,7 @@ namespace Conekta;
 
 abstract class Conekta
 {
-    public static $apiKey;
+    public static $apiKey ;
     public static $apiBase = 'https://api.conekta.io';
     public static $apiVersion = '2.0.0';
     public static $locale = 'es';
@@ -12,37 +12,39 @@ abstract class Conekta
     public static $pluginVersion = '';
     const VERSION = '4.0.1';
 
-    
+
     public static function setApiKey($apiKey)
     {
         self::$apiKey = $apiKey;
     }
-    
+
     public static function setApiVersion($version)
     {
         self::$apiVersion = $version;
     }
-    
+
     public static function setLocale($locale)
     {
         self::$locale = $locale;
     }
-    
+
     public static function setPlugin($plugin = '')
     {
         self::$plugin = $plugin;
     }
-    
+
     public static function setPluginVersion($pluginVersion = '')
     {
         self::$pluginVersion = $pluginVersion;
     }
-    
+
     public static function getPlugin()
     {
         return self::$plugin;
     }
-    
+
+
+
     public static function getPluginVersion()
     {
         return self::$pluginVersion;
