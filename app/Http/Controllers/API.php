7b0d2c5563cca7e985a10fc10afb54d15311605d
rRios15041995 <rodolfo.r1504@gmail.com>
2018-02-22 07:59:22 -0600

    conek

--- app/Http/Controllers/API.php
+++ app/Http/Controllers/API.php
@@ -21,6 +21,48 @@ class API extends Controller
     \Conekta\Conekta::setApiKey("key_ZuD84FNriznv8HHDPzCCoQ");
     \Conekta\Conekta::setApiVersion("2.0.0");
 
+    $valid_order =
+    array(
+        'line_items'=> array(
+            array(
+                'name'        => 'Box of Cohiba S1s',
+                'description' => 'Imported From Mex.',
+                'unit_price'  => 20000,
+                'quantity'    => 1,
+                'sku'         => 'cohb_s1',
+                'category'    => 'food',
+                'tags'        => array('food', 'mexican food')
+                )
+           ),
+          'currency'    => 'mxn',
+          'metadata'    => array('test' => 'extra info'),
+          'charges'     => array(
+              array(
+                  'payment_source' => array(
+                      'type'       => 'oxxo_cash',
+                      'expires_at' => strtotime(date("Y-m-d H:i:s")) + "36000"
+                   ),
+                   'amount' => 20000
+                )
+            ),
+            'currency'      => 'mxn',
+            'customer_info' => array(
+                'name'  => 'John Constantine',
+                'phone' => '+5213353319758',
+                'email' => 'hola@hola.com'
+            )
+        );
+
+try {
+  $order = \Conekta\Order::create($valid_order);
+} catch (\Conekta\ProcessingError $e){
+  echo $e->getMessage();
+} catch (\Conekta\ParameterValidationError $e){
+  echo $e->getMessage();
+} finally (\Conekta\Handler $e){
+  echo $e->getMessage();
+}
+
     $api_key =   \Conekta\Conekta::setApiKey();
     // return $service_id;
     $subcategories = [];
