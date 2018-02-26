387c4489661d22dd6dc70b971a1cf57540340ea5
Gabriel Morales <gabriel.140497@hotmail.com>
2018-02-26 11:27:29 -0600

    Added images

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,12 +16,12 @@
     <link href="{{asset ('css/bootstrap.min.css')}}" rel="stylesheet">
 
     <!-- Custom fonts for this template -->
-    <link href="{{asset ('vendor/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{asset ('css/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
 
     <!-- Plugin CSS -->
-    <link href="{{asset ('vendor/magnific-popup/magnific-popup.css')}}" rel="stylesheet">
+    <link href="{{asset ('css/magnific-popup.css')}}" rel="stylesheet">
 
     <!-- Custom styles for this template -->
     <link href="{{elixir ('css/creative.css')}}" rel="stylesheet">
@@ -133,8 +133,8 @@
       <div class="container-fluid p-0">
         <div class="row no-gutters popup-gallery">
           <div class="col-lg-4 col-sm-6">
-            <a class="portfolio-box" href="{{asset ('assets/img/portfolio/fullsize/1.jpg')}}">
-              <img class="img-fluid" src="{{asset ('assets/img/portfolio/thumbnails/1.jpg')}}" alt="">
+            <a class="portfolio-box" href="{{asset ('assets/img/portfolio/fullsize/(1).jpg')}}">
+              <img class="img-fluid gallery" src="{{asset ('assets/img/portfolio/thumbnails/(1).jpg')}}" alt="">
               <div class="portfolio-box-caption">
                 <div class="portfolio-box-caption-content">
                   <div class="project-category text-faded">
@@ -148,8 +148,8 @@
             </a>
           </div>
           <div class="col-lg-4 col-sm-6">
-            <a class="portfolio-box" href="{{asset ('assets/img/portfolio/fullsize/2.jpg')}}">
-              <img class="img-fluid" src="{{asset ('assets/img/portfolio/thumbnails/2.jpg')}}" alt="">
+            <a class="portfolio-box" href="{{asset ('assets/img/portfolio/fullsize/(2).jpg')}}">
+              <img class="img-fluid gallery" src="{{asset ('assets/img/portfolio/thumbnails/(2).jpg')}}" alt="">
               <div class="portfolio-box-caption">
                 <div class="portfolio-box-caption-content">
                   <div class="project-category text-faded">
@@ -163,8 +163,8 @@
             </a>
           </div>
           <div class="col-lg-4 col-sm-6">
-            <a class="portfolio-box" href="{{asset ('assets/img/portfolio/fullsize/3.jpg')}}">
-              <img class="img-fluid" src="{{asset ('assets/img/portfolio/thumbnails/3.jpg')}}" alt="">
+            <a class="portfolio-box" href="{{asset ('assets/img/portfolio/fullsize/(3).jpg')}}">
+              <img class="img-fluid gallery" src="{{asset ('assets/img/portfolio/thumbnails/(3).jpg')}}" alt="">
               <div class="portfolio-box-caption">
                 <div class="portfolio-box-caption-content">
                   <div class="project-category text-faded">
@@ -178,8 +178,8 @@
             </a>
           </div>
           <div class="col-lg-4 col-sm-6">
-            <a class="portfolio-box" href="{{asset ('assets/img/portfolio/fullsize/4.jpg')}}">
-              <img class="img-fluid" src="{{asset ('assets/img/portfolio/thumbnails/4.jpg')}}" alt="">
+            <a class="portfolio-box" href="{{asset ('assets/img/portfolio/fullsize/(4).jpg')}}">
+              <img class="img-fluid gallery" src="{{asset ('assets/img/portfolio/thumbnails/(4).jpg')}}" alt="">
               <div class="portfolio-box-caption">
                 <div class="portfolio-box-caption-content">
                   <div class="project-category text-faded">
@@ -193,8 +193,8 @@
             </a>
           </div>
           <div class="col-lg-4 col-sm-6">
-            <a class="portfolio-box" href="{{asset ('assets/img/portfolio/fullsize/5.jpg')}}">
-              <img class="img-fluid" src="{{asset ('assets/img/portfolio/thumbnails/5.jpg')}}" alt="">
+            <a class="portfolio-box" href="{{asset ('assets/img/portfolio/fullsize/(5).jpg')}}">
+              <img class="img-fluid gallery" src="{{asset ('assets/img/portfolio/thumbnails/(5).jpg')}}" alt="">
               <div class="portfolio-box-caption">
                 <div class="portfolio-box-caption-content">
                   <div class="project-category text-faded">
@@ -208,8 +208,8 @@
             </a>
           </div>
           <div class="col-lg-4 col-sm-6">
-            <a class="portfolio-box" href="{{asset ('assets/img/portfolio/fullsize/6.jpg')}}">
-              <img class="img-fluid" src="{{asset ('assets/img/portfolio/thumbnails/6.jpg')}}" alt="">
+            <a class="portfolio-box" href="{{asset ('assets/img/portfolio/fullsize/(6).jpg')}}">
+              <img class="img-fluid gallery" src="{{asset ('assets/img/portfolio/thumbnails/(6).jpg')}}" alt="">
               <div class="portfolio-box-caption">
                 <div class="portfolio-box-caption-content">
                   <div class="project-category text-faded">
@@ -288,16 +288,16 @@
     </footer>
 
     <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="{{asset ('js/jquery.min.js')}}"></script>
     <script src="{{asset ('js/bootstrap.bundle.min.js')}}"></script>
 
     <!-- Plugin JavaScript -->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
-    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+    <script src="{{asset ('js/jquery.easing.min.js')}}"></script>
+    <script src="{{asset ('js/scrollreveal.min.js')}}"></script>
+    <script src="{{asset ('js/jquery.magnific-popup.min.js')}}"></script>
 
     <!-- Custom scripts for this template -->
-    <script src="js/creative.min.js"></script>
+    <script src="{{asset ('js/creative.min.js')}}"></script>
 
   </body>
 
