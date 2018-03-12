c6fbc2be57d4793b0bab9279546a6e23cf401c42
rRios15041995 <rodolfo.r1504@gmail.com>
2018-03-12 13:22:20 -0600

    HIDDE SECTION

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -218,7 +218,7 @@
       </div>
     </section>
 
-    <section id="about" class="bg-dark">
+    <!-- <section id="about" class="bg-dark">
       <div class="container">
           <div class="row">
             <div class="col-md-4 team-box">
@@ -253,7 +253,7 @@
             </div>
           </div>
       </div>
-    </section>
+    </section> -->
     <div class="map" style="width=100%; height="350px;">
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d225.02268788588222!2d-103.41687218545623!3d25.526282475319263!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x868fdb9004663a09%3A0x205abd48886a3634!2sCARLAB+Carwash+Grua+Vulka+A+Domicilio!5e0!3m2!1ses!2smx!4v1520881223972" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen=""></iframe>
     </div>
