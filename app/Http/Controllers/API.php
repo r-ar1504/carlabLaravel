6b9f1ada68ed2385b672b27d0343d7350a114e01
rRios15041995 <rodolfo.r1504@gmail.com>
2018-03-28 12:13:21 -0600

    ;

--- app/Http/Controllers/API.php
+++ app/Http/Controllers/API.php
@@ -13,14 +13,14 @@ class API extends Controller
     $data = $req->all();
     $order = DB::table('Order')->where('id','=',$data['order_id'])->first();
 
-    $total_distance = $this->getServiceDistance($order->latitude, $order->longitude, $data[latitude], $data[latitude], $earthRadius = 6371000)
+    $total_distance = $this->getServiceDistance($order->latitude, $order->longitude, $data[latitude], $data[latitude], $earthRadius = 6371000);
 
     if ($total_distance < 3000) {
       Pusher::trigger("worker-".$data['worker_id'], "debug-loc", ['distance' => $order_data]);
-      return "OK;"
+      return "OK;";
     }
 
-    return "OK;"
+    return "OK;";
 
   }
 
