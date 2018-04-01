b260144a8af8bc5e4f0633a4d9ddd3f8cba156d4
rRios15041995 <rodolfo.r1504@gmail.com>
2018-04-01 16:10:48 -0500

    testing

--- app/Http/Controllers/API.php
+++ app/Http/Controllers/API.php
@@ -7,7 +7,30 @@ use Pusher\Laravel\Facades\Pusher;
 
 class API extends Controller
 {
+  function testing(){
+    $orders = DB::table('Order')->where('status', '=', 0)->get();
 
+    if ( count($orders) > 0) {
+
+      foreach ($orders as $order) {
+        $c_o = $order->id;
+        if ($order->rejections < 2) {
+          $closest=DB::table('OrderCandidate')->where('order_id','=',$c_o)->min('service_distance');
+
+          $worker = DB::table('OrderCandidate')->where('order_id','=',$c_o)->where('service_distance', $closest)->first();
+
+            Pusher::trigger('worker-'.$worker->worker_id, 'new-order', ['order' => $order]);
+        }else{
+            $message = "No hay operadores disponibles por el momento";
+            Pusher::trigger('order-'.$order->id, 'no-workers', ['message' => $message] );
+             /*Delete order from DB*/
+            DB::table('Order')->where('id', $order->id);
+        }
+      }
+    }else {
+      echo "No Pending Orders";
+    }
+  }
   //<!--[Report Location]-->//
   function reportLocation(Request $req){
     $data = $req->all();
