e29978aea71e20f42039456a3be7e0ccb131964f
rRios15041995 <rodolfo.r1504@gmail.com>
2018-04-01 17:37:10 -0500

    sav

--- app/Http/Controllers/API.php
+++ app/Http/Controllers/API.php
@@ -18,7 +18,7 @@ class API extends Controller
         if ($order->rejections < 2) {
           $closest=DB::table('OrderCandidate')->where('order_id','=',$c_o)->min('service_distance');
 
-          $worker = DB::table('OrderCandidate')->where('order_id','=',$c_o)->where('service_distance', $closest)->first();
+          $worker = DB::table('OrderCandidate')->where('worker_response', '!=', 0)->where('order_id','=',$c_o)->where('service_distance', $closest)->first();
 
             Pusher::trigger('worker-'.$worker->worker_id, 'new-order', ['order' => $order]);
         }else{
